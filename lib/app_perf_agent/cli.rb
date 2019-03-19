b3e0bde453549cf19f3cc91fa967ec2e75ba8888
Vladislav Volkov <uladzislau.volkau@itibo.com>
2019-03-19 08:55:35 +0300

    fix daemonize for -f --foreground option
    agent doesn't start properly in daemon mode because of false daemon in default options and in -f option. Added true to daemon option.

--- lib/app_perf_agent/cli.rb
+++ lib/app_perf_agent/cli.rb
@@ -70,7 +70,7 @@ module AppPerfAgent
         o.banner = "app_perf_agent [options]"
 
         o.on '-f', '--foreground', "Daemonize process" do |arg|
-          opts[:daemon] = false
+          opts[:daemon] = true
         end
 
         o.on '-l', '--license-key LICENSE_KEY', "License Key" do |arg|
